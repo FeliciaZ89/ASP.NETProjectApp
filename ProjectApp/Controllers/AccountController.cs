9ef72643fa2a50cefb73fac9891f3bb5bbcb4806
felyc <fely.coman@gmail.com>
2024-03-16 13:02:37 +0100

    fixed footer for singin/signup signin init

--- ProjectApp/Controllers/AccountController.cs
+++ ProjectApp/Controllers/AccountController.cs
@@ -4,21 +4,16 @@ namespace ProjectApp.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
-        {
-            ViewData["Title"] = "Profile";
-            return View();
-        }
-
+        [Route("/signup")]
         public IActionResult SignIn()
         {
-            ViewData["Title"] = "Sign In";
+           
             return View();
         }
 
         public IActionResult SignUp()
         {
-            ViewData["Title"] = "Sign Up";
+           
             return View();
         }
 
