c88bc044d7cdc48e8e5ce9ff343389c0a931306c
felyc <fely.coman@gmail.com>
2024-03-18 10:56:36 +0100

    sign in form complete

--- ProjectApp/Controllers/AccountController.cs
+++ ProjectApp/Controllers/AccountController.cs
@@ -28,11 +28,37 @@ namespace ProjectApp.Controllers
 
 
 
+        [Route("/signin")]
+        [HttpGet]
+
         public IActionResult SignIn()
         {
-
-            return View();
+            var viewModel = new SignInViewModel();
+            return View(viewModel);
         }
+
+        [Route("/signin")]
+        [HttpPost]
+        public IActionResult SignIn(SignInViewModel viewModel)
+        {
+           
+
+            if (!ModelState.IsValid)
+ 
+                return View(viewModel);
+
+
+            //var result=_accountService.SignIn(viewModel.Form);
+            //if(result)
+            //  return RedirectToAction("Account", "Index");
+
+            viewModel.ErrorMessage = "Incorrect email address or password";
+            return View(viewModel);
+
+          
+
+        }
+
 
 
 
---
+++ ProjectApp/ViewModels/SignInViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ProjectApp.Models;
+
+namespace ProjectApp.ViewModels;
+
+public class SignInViewModel
+{
+    public string Title { get; set; } = "Sign In";
+    public SignInModel Form { get; set; } = new SignInModel();
+    public string? ErrorMessage { get; set; }
+
+}
